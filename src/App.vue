67e8a451f88e59bc03a55faeeb9530119711c2de
yuyuedeluo <lophis120507@gmail.com>
2025-11-07 23:57:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from 'vue'
+import NaviBar from './components/NaviBar.vue'
+
+onMounted(() => {
+  console.log('App component mounted')
+})
+</script>
+
+<template>
+  <div id="app">
+    <NaviBar />
+    <main>
+      <h1>Welcome to the Vue.js Application</h1>
+      <p>This is the main content area.</p>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
---
+++ src/components/NaviBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navi-bar">
+    <ul class="navi-list">
+      <li class="navi-item"><a href="#home">Home</a></li>
+      <li class="navi-item"><a href="#about">About</a></li>
+      <li class="navi-item"><a href="#services">Services</a></li>
+      <li class="navi-item"><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.navi-bar {
+  background-color: #000000;
+  padding: 2rem;
+}
+.navi-list {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  margin: 0;
+  padding: 0;
+}
+.navi-item a {
+  color: rgb(255, 0, 0);
+  text-decoration: none;
+}
+</style>
